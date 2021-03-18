4df8bf6074e920ecbd144f93ad9f9866692d6eab
GG <jerometonnelier@yahoo.fr>
2021-03-18 15:01:27 +0100

    public manager

--- Sources/ATACommonObjects/Managers/RideDirectionManager.swift
+++ Sources/ATACommonObjects/Managers/RideDirectionManager.swift
@@ -10,9 +10,9 @@ import SwiftLocation
 import MapKit
 import MapExtension
 
-struct RideDirections: Directions {
-    var id: String { "\(ride.id)" }
-    var directions: [Direction] {
+public struct RideDirections: Directions {
+    public var id: String { "\(ride.id)" }
+    public var directions: [Direction] {
         var dir: [RideDirection] = []
         if let userLocation = SwiftLocation.lastKnownGPSLocation?.coordinate {
             dir.append(RideDirection(id: RideDirection.start, startLocation: userLocation, endLocation: ride.fromAddress.asCoordinates2D))
@@ -24,25 +24,25 @@ struct RideDirections: Directions {
     }
     
     var ride: BaseRide!
-    init(ride: BaseRide) {
+    public init(ride: BaseRide) {
         self.ride = ride
     }
 }
 
 extension DirectionsAnswer {
-    var routes: [Route] {
+    public var routes: [Route] {
         directions.compactMap { (key, route) -> Route? in
             Route(routeType: key == RideDirection.start ? .approach : .ride, route: route)
         }
     }
 }
 
-struct RideDirection: Direction {
+public struct RideDirection: Direction {
     fileprivate static let start = "start"
     fileprivate static let end = "end"
-    var id: String
-    var startLocation: CLLocationCoordinate2D
-    var endLocation: CLLocationCoordinate2D
+    public var id: String
+    public var startLocation: CLLocationCoordinate2D
+    public var endLocation: CLLocationCoordinate2D
 }
 
 public struct Route {
@@ -57,16 +57,16 @@ public struct Route {
     }
 }
 
-class RideDirectionManager {
-    typealias RouteCompletion = ((_ ride: BaseRide, _ routes: [Route]) -> Void)
-    static let shared: RideDirectionManager = RideDirectionManager()
+public class RideDirectionManager {
+    public typealias RouteCompletion = ((_ ride: BaseRide, _ routes: [Route]) -> Void)
+    public static let shared: RideDirectionManager = RideDirectionManager()
     private var routes: [Ride: [Route]] = [:]
     private init() {}
     
     private var isLocationActive: Bool { SwiftLocation.authorizationStatus == .authorizedWhenInUse || SwiftLocation.authorizationStatus == .authorizedAlways }
     private var loadQueue: DispatchQueue = DispatchQueue(label: "LoadRoutes", qos: .default)
     
-    func loadDirections(for ride: BaseRide, completion: @escaping RouteCompletion) {
+    public func loadDirections(for ride: BaseRide, completion: @escaping RouteCompletion) {
         guard isLocationActive else { return }
         DirectionManager
             .shared
