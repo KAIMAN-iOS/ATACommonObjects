6ae1d42f2e5591b3ad1acf932944eebf13a419b2
jerometonnelier <j.tonnelier@kaiman.fr>
2022-01-07 14:48:45 +0100

    SwiftyUserDefaults

--- Package.swift
+++ Package.swift
@@ -21,13 +21,14 @@ let package = Package(
         .package(url: "https://github.com/KAIMAN-iOS/KStorage", .branch("master")),
         .package(url: "https://github.com/malcommac/SwiftLocation", from: "5.1.0"),
         .package(url: "https://github.com/KAIMAN-iOS/PhoneNumberKit", from: "4.0.0"),
+        .package(url: "https://github.com/sunshinejr/SwiftyUserDefaults", from: "5.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ATACommonObjects",
-            dependencies: ["KExtensions", "ATAConfiguration", "SwiftLocation", "PhoneNumberKit", "KStorage", "NSAttributedStringBuilder"],
+            dependencies: ["KExtensions", "ATAConfiguration", "SwiftLocation", "PhoneNumberKit", "KStorage", "NSAttributedStringBuilder", "SwiftyUserDefaults"],
             resources: [.process("Villes/CP-FR.txt")])
     ]
 )
